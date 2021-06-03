199dccd61cd7985a6593cd803dcb3cdc36bf9a3b
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-06-03 14:42:13 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -14,12 +14,14 @@ namespace ass_2
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -27,6 +29,22 @@ namespace ass_2
             services.AddControllersWithViews();
 			
 			services.AddSingleton<IHelper, Helper>();
+                
+                 services.addDbContext(options => {
+                   var connectionString = Configuration.GetConnectionString("ass_2Context");
+            /**
+            if (Environment.IsDevelopment())
+            {
+                options.UseSqlite(connectionString);
+            }
+            else
+            {
+                options.UseSqlServer(connectionString);
+            }
+            **/
+                     options.UseSqlServer(connectionString); 
+                 });
+                
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
